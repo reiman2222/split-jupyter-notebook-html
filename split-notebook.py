fbf6bc394bbb23c0d64c3a0c51760f1119ea6595
Zachary Catalano <catalanoz15@students.ecu.edu>
2018-10-01 15:37:53 -0400

    Added custom html for each topic

--- split-notebook.py
+++ split-notebook.py
@@ -1,153 +1,172 @@
-#This program splits a large jupyter notebook into individual jupyter notebooks 
+#This program splits a large jupyter notebook into individual jupyter notebooks
 #based on first level heading. The first cell is assumed to be the table of contents and is removed.
 #REQUIRES all fisrt level headings to have a tag of the form: <a id='someID'></a>
 
 import json
 import re
 import copy
+import os
+from lxml import html
 
 def getIDFromCellSource(cell):
-	if(cell['cell_type'] != 'markdown'):
-		return None
-	else:
-		cellSource = cell['source']
-		for line in cellSource:
-			if(line != '' and '<a id=\'' in line):
-				lineL = re.split('<a id=\'', line)
-				lineL = lineL[1].split("'")
-				lineID = lineL[0]
+    if(cell['cell_type'] != 'markdown'):
+        return None
+    else:
+        cellSource = cell['source']
+        for line in cellSource:
+            if(line != '' and '<a id=\'' in line):
+                lineL = re.split('<a id=\'', line)
+                lineL = lineL[1].split("'")
+                lineID = lineL[0]
 
-				return lineID
-		return None
+                return lineID
+        return None
 
 #returns a list of tuples of the form (startCellIndex, endCellIndex, cellId)
 def getSubNotebooks(cells):
-	i = 0
-	startCell = 0
-	endCell = 0
+    i = 0
+    startCell = 0
+    endCell = 0
 
-	lookingForEnd = False
+    lookingForEnd = False
 
-	outputList = []
+    outputList = []
 
-	cellID = ''
+    cellID = ''
 
-	while(i < len(cells)):
-		cell = cells[i]
-		if(isLevel1Header(cell) and not lookingForEnd):
-			startCell = i
-			lookingForEnd = True
+    while(i < len(cells)):
+        cell = cells[i]
+        if(isLevel1Header(cell) and not lookingForEnd):
+            startCell = i
+            lookingForEnd = True
 
-			cellID = getIDFromCellSource(cell)
+            cellID = getIDFromCellSource(cell)
 
-		elif((isLevel1Header(cell) or ((i + 1)  == len(cells))) and lookingForEnd):
-			endCell = i 
-			i = i - 1
-			lookingForEnd = False
+        elif((isLevel1Header(cell) or ((i + 1)  == len(cells))) and lookingForEnd):
+            endCell = i
+            i = i - 1
+            lookingForEnd = False
 
-			outputList.append((startCell, endCell, cellID))
+            outputList.append((startCell, endCell, cellID))
 
-		i += 1
-	return outputList
+        i += 1
+    return outputList
 
 #returns true if cell has a level 1 header
 def isLevel1Header(cell):
-	if(cell['cell_type'] != 'markdown'):
-		return False
+    if(cell['cell_type'] != 'markdown'):
+        return False
 
-	cellSource = cell['source']
-	if(len(cellSource) > 0):
-		matches = re.match('^# ', cellSource[0])
-		if(matches != None):
-			return True
-		else: 
-			return False
+    cellSource = cell['source']
+    if(len(cellSource) > 0):
+        matches = re.match('^# ', cellSource[0])
+        if(matches != None):
+            return True
+        else:
+            return False
 
-	return False
+    return False
 
-
+def checkDir(dir_name):
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
 
 def writeSubNotebooks(subNotebooks, js, folderToWriteTo):
 
-	for sn in subNotebooks:
-		jsCopy = copy.deepcopy(js)
+    for sn in subNotebooks:
+        jsCopy = copy.deepcopy(js)
 
 
-		start = sn[0]
-		end = sn[1]
-		fname = sn[2]
-		
-		length = len(jsCopy['cells'])
+        start = sn[0]
+        end = sn[1]
+        fname = sn[2]
 
-		if(start == 0):
-			del jsCopy['cells'][end:]
-		else:
-			del jsCopy['cells'][end:]
-			del jsCopy['cells'][:start]
+        length = len(jsCopy['cells'])
 
-		with open(folderToWriteTo + '/' + fname + '.ipynb', 'w+') as outfile:
-			json.dump(jsCopy,  outfile)
-		
+        if(start == 0):
+            del jsCopy['cells'][end:]
+        else:
+            del jsCopy['cells'][end:]
+            del jsCopy['cells'][:start]
 
+        checkDir(folderToWriteTo)
 
+        with open(folderToWriteTo + '/' + fname + '.ipynb', 'w+') as outfile:
+            json.dump(jsCopy,  outfile)
+
+def formatHTML(body):
+
+    page_html = html.fromstring(body)
+
+    # Remove all style elements
+    for style in page_html.xpath('//style[@type="text/css"]'):
+          style.getparent().remove(style)
+
+    # link to the style file
+    style_custom = html.Element('link', href="css/notebook.css", rel="stylesheet")
+    page_html.xpath('//meta')[0].getparent().append(style_custom)
+
+    # set base location
+    base_loc = html.Element('base', href="../")
+    page_html.xpath('//meta')[0].getparent().insert(1, base_loc)
+
+    # Change custom.css to correct location
+    page_html.xpath('//link[@href="custom.css"]')[0].attrib['href'] = "css/custom.css"
+
+    return html.tostring(page_html,encoding='utf8')
 
 #-----------------------------------#
 #               MAIN                #
 #-----------------------------------#
 
+def main():
+    # notebookName = input('Enter the name of the notebook you would like convert to html:\n')
+    notebookName = "classification.ipynb"
+
+    f = open(notebookName,'r')
+    js = json.loads(f.read())
+
+    cells = js['cells']
+
+    tableOfContentsCell = cells[0]
+
+    tableOfCSource = tableOfContentsCell['source']
+
+    del cells[0]
+
+    subNotebooks = getSubNotebooks(cells)
+
+    print('\nThe following topics have be identified:')
+    for tup in subNotebooks:
+        print(tup[2])
 
 
-notebookName = input('Enter the name of the notebook you would like convert to html:\n')
+    writeSubNotebooks(subNotebooks, js, 'topics')
 
+    print('\nWritting results to folder: html')
 
-f = open(notebookName,'r')
-js = json.loads(f.read())
+    import nbformat
+    from nbconvert.preprocessors import ExecutePreprocessor
 
-cells = js['cells']
+    from nbconvert import HTMLExporter
 
-tableOfContentsCell = cells[0]
+    notebook_filename = "classification.ipynb"
 
-tableOfCSource = tableOfContentsCell['source']
+    for topic in subNotebooks:
+        filename = topic[2]
+        notebook_filename = 'topics/' + filename + '.ipynb'
 
-del cells[0]
+        with open(notebook_filename) as f:
+            nb = nbformat.read(f, as_version=4)
 
+        html_exporter = HTMLExporter()
+        html_exporter.template_file = 'full'
 
+        (body, resources) = html_exporter.from_notebook_node(nb)
 
-subNotebooks = getSubNotebooks(cells)
+        checkDir('html')
 
-print('\nThe following topics have be identified:')
-for tup in subNotebooks:
-	print(tup[2])
+        with open('html/' + filename + '.ejs', "w+") as nbHTML:
+            nbHTML.write(formatHTML(body))
 
-
-writeSubNotebooks(subNotebooks, js, 'topics')
-
-print('\nWritting results to folder: html')
-
-
-
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
-
-from nbconvert import HTMLExporter
-
-notebook_filename = "classification.ipynb"
-
-for topic in subNotebooks:
-	filename = topic[2]
-	notebook_filename = 'topics/' + filename + '.ipynb'
-
-	with open(notebook_filename) as f:
-		nb = nbformat.read(f, as_version=4)
-
-	html_exporter = HTMLExporter()
-	html_exporter.template_file = 'full'
-
-	(body, resources) = html_exporter.from_notebook_node(nb)
-
-	with open('html/' + filename + '.html', "w+") as nbHTML:
-		nbHTML.write(body)
-
-
-
-
+main()
